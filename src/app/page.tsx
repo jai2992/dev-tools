e9c9f3279e35f6f32ee4a0b304f59b9d014925f6
JAIGURU <136327019+jai2992@users.noreply.github.com>
2025-07-25 09:41:46 +0000

    changes to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div className="flex h-screen w-screen font-bold text-white bg-black justify-center items-center">
       Vanakam da maple nBase2 la irunthu 🙏
+      just trying to deploy website
     </div>
   );
 }
