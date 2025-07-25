91660e21f12f85d0c3038fda38850c8e77d5b60a
JAIGURU <136327019+jai2992@users.noreply.github.com>
2025-07-25 09:50:45 +0000

    removed adsence

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4913229567533880"
-        crossOrigin="anonymous"></script>
-      </head>
       <body>
         {children}
       </body>
