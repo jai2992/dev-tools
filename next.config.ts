ae9b8c8e7f9037e5e29c7bdf1777fb70c0603a0a
JAIGURU <136327019+jai2992@users.noreply.github.com>
2025-07-25 09:09:49 +0000

    removed gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: isProd ? '/dev-tools/' : '',
-  basePath: isProd ? '/dev-tools' : '',
 };
 
 export default nextConfig;
